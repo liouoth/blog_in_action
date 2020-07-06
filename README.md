8ca7af2217a8504ffcc9145e5c0b6709611efcc7
maowenqing <450787674@qq.com>
2020-07-06 18:07:20 +0800

    mwq_add_thymeleaf

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # blog_in_action
 build a website service of blog step by step
+
+跟着老卫的慕课视频节奏，自己写一个博客网站
+
+2020/7/6 学着整合工具
+
+- 整合thymelea
+- 整合elastic search、jpa
+- 整合spring security
+- 整合bootstrap
+
---
+++ thymeleaf_in_action/src/main/java/com/leo/thymeleaf_in_action/ThymeleafInActionApplication.java
@@ -0,0 +1,13 @@
+package com.leo.thymeleaf_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafInActionApplication.class, args);
+	}
+
+}
