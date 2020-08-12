688f77be841b99d26bb3a67644c2924ed26136cd
liouoth <mwqyykl@gmail.com>
2020-08-13 00:43:05 +0800

    springboot framework

---
+++ blog/src/main/java/com/will/blog/handler/ControllerException.java
@@ -0,0 +1,25 @@
+package com.will.blog.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+
+public class ControllerException {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e){
+        logger.error("Request URL :{},Exception:{}",request.getRequestURL(),e.getMessage());
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url",request.getRequestURL());
+        mv.addObject("exception",e);
+        mv.setViewName("error/error");
+        return mv;
+    }
+}
